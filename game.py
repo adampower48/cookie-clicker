dcd3e46802ab1c091423515f2397bed00cb15983
unknown <adam.power48@mail.dcu.ie>
2019-08-19 21:46:55 +0100

    Add basic upgrades: additive/multiplicative producer upgrades.  Add available action getter.  Add random action function.

--- game.py
+++ game.py
@@ -1,6 +1,41 @@
+import random
+
+class Upgrade:
+    def __init__(self, name, cost):
+        self.name = name
+        self.cost = cost
+        
+        self.owned = False
+        
+    def buy(self):
+        self.owned = True
+        
+class ProducerMultiplierUpgrade(Upgrade):
+    def __init__(self, name, cost, producer, multiplier):
+        super().__init__(name, cost)
+        self.producer = producer
+        self.multiplier = multiplier
+        
+    def buy(self):
+        super().buy()
+        self.producer.multiplier *= self.multiplier
+        
+        
+class ProducerAdditiveUpgrade(Upgrade):
+    def __init__(self, name, cost, producer, add_amount):
+        super().__init__(name, cost)
+        self.producer = producer
+        self.add_amount = add_amount
+        
+    def buy(self):
+        super().buy()
+        self.producer.add_pre += self.add_amount
+
+
 class CookieClickerGame:
     producer_spec = [
-        ("cursor",              0.1,    15,     1.15),
+        # name, production, cost, price_scaling
+        ("cursor",              0.1,    15,     1.15), # todo: scaling
         ("grandma",             1,      100,    1.15),
         ("farm",                8,      1100,   1.15),
         ("mine",                47,     12e3,   1.15),
@@ -17,6 +52,10 @@ class CookieClickerGame:
         ("chancemaker",         21e9,   26e15,  1.15),
         ("fractal_engine",      150e9,  310e15, 1.15),
     ]
+    
+    upgrade_spec = [
+        dict(type=ProducerMultiplierUpgrade, name="clicker1", cost=100, producer="cursor", multiplier=2)
+    ]
 
     def __init__(self):
         self.cookies = 0
@@ -24,17 +63,50 @@ class CookieClickerGame:
         self.cpt = 0
         
         self.producers = self._setup_producers()
+        self.upgrades = self._setup_upgrades()
         
     def _setup_producers(self):
         return [Producer(*spec) for spec in self.producer_spec]
+        
+    def _setup_upgrades(self):
+        upgrades = []
+        for spec in self.upgrade_spec:
+            if "producer" in spec and type(spec["producer"]) == str:
+                spec["producer"] = self.get_producer(spec["producer"])
+            
+            upgrades.append(spec["type"](**{k:v for k,v in spec.items() if k != "type"}))
+            
+        return upgrades
     
     def get_cpt(self):
         return sum(p.production for p in self.producers)
+        
+    def get_producer(self, name):
+        return next(p for p in self.producers if p.name == name)
     
     def advance(self):
         self.cpt = self.get_cpt()
         self.cookies += self.cpt
         self.turn += 1
+        
+    def buy_upgrade(self, idx):
+        upgrade = self.upgrades[idx]
+        cost = upgrade.cost
+        
+        if upgrade.owned:
+            print(f"Upgrade: {upgrade.name} already owned.")
+            return False
+        
+        if cost <= self.cookies:
+            print(f"Bought: {upgrade.name} for {upgrade.cost}. Cookies: {self.cookies} -> ", end="")
+            self.cookies -= cost
+            upgrade.buy()
+            print(f"{self.cookies:.1f}")
+            
+            return True
+        else:
+            print(f"Cant afford upgrade: {upgrade.name} for {upgrade.cost:.0f} with {self.cookies:.1f}")
+            return False
         
     def buy_producer(self, idx):
         prod = self.producers[idx]
@@ -63,7 +135,7 @@ class CookieClickerGame:
             return True
         
         else:
-            print("Cant sell:", prod.name, ". Owned:", prod.n_owned)
+            print(f"Cant sell: {prod.name}. Owned: {prod.n_owned}")
             return False
             
             
@@ -73,9 +145,63 @@ class CookieClickerGame:
     def __str__(self):
         return "\n".join([
             f"Turn: {self.turn}, Cookies: {self.cookies:.1f}, Producing: {self.cpt:.1f}",
-            "\n".join(f"{p.name} - Owned: {p.n_owned}, Cost: {p.current_price:.0f}, Producing: {p.production:.1f}" for p in self.producers)
+            "Producers:",
+            "\n".join(f"{p.name} - Owned: {p.n_owned}, Cost: {p.current_price:.0f}, Producing: {p.production:.1f}" for p in self.producers),
+            "Upgrades:",
+            "\n".join(f"{u.name} - Type: {u.__class__.__name__}" for u in self.upgrades if u.owned),
         ])
         
+    def get_available_actions(self):   
+        def _pass():
+            pass
+    
+        actions = []
+        
+        actions.append([_pass])
+        
+        actions.append([self.click])
+        
+        avail_buy_prod = [[i] for i, p in enumerate(self.producers) if p.current_price <= self.cookies]
+        actions.append([self.buy_producer, avail_buy_prod])
+        
+        avail_sell_prod = [[i] for i, p in enumerate(self.producers) if p.n_owned > 0]
+        actions.append([self.sell_producer, avail_sell_prod])
+        
+        avail_buy_upgr = [[i] for i, u in enumerate(self.upgrades) if not u.owned and u.cost <= self.cookies]
+        actions.append([self.buy_upgrade, avail_buy_upgr])
+            
+        return actions
+    
+    def random_action(self, weights=None):
+        """
+        weights: [pass, click, buy_prod, sell_prod, buy_upgr]
+        
+        """
+    
+        def update_weights(weights):
+            if weights is None:
+                weights = [1, 1, 1, 1, 1]
+        
+            for i, action in enumerate(avail_actions):
+                if len(action) > 1 and len(action[1]) == 0:
+                    weights[i] = 0
+            
+            return weights
+
+
+            
+    
+        avail_actions = self.get_available_actions()
+        weights = update_weights(weights)
+        
+        action = random.choices(avail_actions, weights, k=1)[0]
+        if len(action) == 1:
+            action[0]()
+        else:
+            args = random.choice(action[1])
+            action[0](*args)
+            
+    
     
 class Producer:
     def __init__(self, name, cpt, base_price, price_scaling):
@@ -87,6 +213,12 @@ class Producer:
         self.n_owned = 0
         self.current_price = base_price
         self.production = 0
+        self.multiplier = 1
+        self.add_pre = 0
+        self.add_post = 0
+    
+    def get_production(self):
+        return (self.cpt + self.add_pre) * self.n_owned * self.multiplier + self.add_post
     
     def get_price(self, n):
         return int(self.base_price * self.price_scaling ** n)
@@ -94,10 +226,16 @@ class Producer:
     def buy(self):
         self.n_owned += 1
         self.current_price = self.get_price(self.n_owned)
-        self.production = self.cpt * self.n_owned
+        self.production = self.get_production()
         
     def sell(self):
         self.n_owned -= 1
         self.current_price = self.get_price(self.n_owned)
-        self.production = self.cpt * self.n_owned
+        self.production = self.get_production()
+ 
+        
+    
+        
+        
+        
         
--- main.py
+++ main.py
@@ -2,17 +2,25 @@ from game import CookieClickerGame
 import random
 
 game = CookieClickerGame()
-game.cookies = 100
+game.cookies = 50
 
-for i in range(100):
+for i in range(1000):
     print(game)
     print("---")
     
-    idx = random.randrange(len(game.producers))
-    if random.random() < 0.9:
-        game.buy_producer(idx)
-    else:
-        game.sell_producer(idx)
+    
+    game.random_action(weights=[0, 10, 1, 0, 1])
+    
+    
+    # if random.random() < 0.5:
+        # idx = random.randrange(len(game.upgrades))
+        # game.buy_upgrade(idx)
+    # else:
+        # idx = random.randrange(len(game.producers))
+        # if random.random() < 0.9:
+            # game.buy_producer(idx)
+        # else:
+            # game.sell_producer(idx)
         
     game.advance()
     
